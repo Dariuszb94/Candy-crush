be1294b0537ffc7c390bd2f1468053eb5005ab1f
Dariuszb94 <59793846+Dariuszb94@users.noreply.github.com>
2022-08-21 20:52:35 +0200

    feat: setCurrentColorArrangement

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import { useEffect, useState } from 'react';
+
 const width = 8;
 const candyColors = ['blue', 'green', 'orange', 'purple', 'red', 'yellow'];
 
 const App = () => {
+  const [currentColorArrangement, setCurrentColorArrangement] = useState([]);
+
   const createBoard = () => {
     const randomColorArrangement = [];
     for (let i = 0; i < width * width; i++) {
@@ -9,7 +13,12 @@ const App = () => {
         candyColors[Math.floor(Math.random() * candyColors.length)];
       randomColorArrangement.push(randomColor);
     }
+    setCurrentColorArrangement(randomColorArrangement);
   };
+
+  useEffect(() => {
+    createBoard();
+  }, []);
   return <div></div>;
 };
 
