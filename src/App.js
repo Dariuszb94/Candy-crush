58473e57c8b4b2bebad9c06f24a09d24463a8c1b
Dariuszb94 <59793846+Dariuszb94@users.noreply.github.com>
2022-09-20 16:21:26 +0200

    fix: exclude blanks

--- src/App.js
+++ src/App.js
@@ -27,10 +27,11 @@ const App = () => {
     for (let i = 0; i <= 39; i++) {
       const columnOfFour = [i, i + width, i + width * 2, i + width * 3];
       const decidedColor = currentColorArrangement[i];
-
+      const isBlank = currentColorArrangement[i] === blank;
       if (
         columnOfFour.every(
-          (square) => currentColorArrangement[square] === decidedColor
+          (square) =>
+            currentColorArrangement[square] === decidedColor && !isBlank
         )
       ) {
         setScoreDisplay((score) => score + 4);
